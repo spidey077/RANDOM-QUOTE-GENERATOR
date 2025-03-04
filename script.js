3722cf024765754a6df029ec9d76ee8e6ae69097
Imdadullah <137488875+spidey077@users.noreply.github.com>
2025-03-04 21:14:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const getQuote = async () => {
     let message = document.getElementById('quoteMessage');
+    message.classList.remove('show');
 
     try {
         let response = await fetch('https://dummyjson.com/quotes/random');
         let data = await response.json();
-        message.innerText = `"${data.quote}" — ${data.author}`;
+        message.innerHTML = `"${data.quote}" <br><span class="author"> ${data.author}</span>`;
+        setTimeout(() => {
+            message.classList.add('show');
+        }, 200);
+
     } catch (error) {
-        message.innerText = 'Failed to fetch quote. Try again!';
+        message.innerHTML = 'Failed to fetch quote. Try again!';
+        message.classList.add('show');
     }
 };
 
