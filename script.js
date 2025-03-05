3a1616a1a0d392945d580afd8909dfc5611612dd
Imdadullah <137488875+spidey077@users.noreply.github.com>
2025-03-05 19:35:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const getQuote = async () => {
+document.addEventListener("DOMContentLoaded", function () {
     let message = document.getElementById('quoteMessage');
-    message.classList.remove('show');
+    message.innerHTML = 'Click the button to display a quote';
+    message.classList.add('show');
+    const getQuote = async () => {
+        message.classList.remove('show');
 
-    try {
-        let response = await fetch('https://dummyjson.com/quotes/random');
-        let data = await response.json();
-        message.innerHTML = `"${data.quote}" <br><span class="author"> ${data.author}</span>`;
-        setTimeout(() => {
+        try {
+            let response = await fetch('https://dummyjson.com/quotes/random');
+            let data = await response.json();
+            message.innerHTML = `"${data.quote}" <br><span class="author"> ${data.author}</span>`;
+            setTimeout(() => {
+                message.classList.add('show');
+            }, 200);
+
+        } catch (error) {
+            message.innerHTML = 'Failed to fetch quote. Try again!';
             message.classList.add('show');
-        }, 200);
+        }
+    };
 
-    } catch (error) {
-        message.innerHTML = 'Failed to fetch quote. Try again!';
-        message.classList.add('show');
-    }
-};
-
-document.getElementById('quoteBtn').addEventListener('click', getQuote);
+    document.getElementById('quoteBtn').addEventListener('click', getQuote);
+});
